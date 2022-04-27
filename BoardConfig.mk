060d2aa21b70c774475cfe29eead9ca547e5414d
horoid <59495159+horoidrom@users.noreply.github.com>
2022-04-27 10:53:27 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,3 +79,15 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+SHRP_DEVICE_CODE := 7306F
+SHRP_PATH := device/lenovo/7306F
+SHRP_MAINTAINER := smily
+SHRP_DEVICE_TYPE := A/B
+SHRP_AB := true
+SHRP_OTG := /usb_otg
+SHRP_EXTERNAL := /external_sd
+SHRP_LITE := true
+
+
+
