bdfa7db51566c4c5ecad8d3ca9821effad582631
horoid <59495159+horoidrom@users.noreply.github.com>
2022-04-27 16:58:15 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -24,6 +24,27 @@ AB_OTA_PARTITIONS += \
     system \
     vendor
 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6761
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6761 \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
